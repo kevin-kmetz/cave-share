30d6d866839b13e34b9f8b069e85349d3c58374a
Kevin Kmetz <kev.kmetz@gmail.com>
2017-02-22 17:57:47 +0800

    Made a skeleton template for the utility.

---
+++ CaveShare.java
@@ -0,0 +1,35 @@
+// cave-share
+// A very crude and primitive file transfer utility.
+// Programmed by Kevin Kmetz
+
+import java.net.*;
+
+class CaveShare {
+
+	static void CaveServer(){
+
+		// Add code for the server here.
+
+	}
+
+	static void CaveClient(){
+
+		// Add code for the client here.
+
+	}
+
+	public static void main(String args[]) {
+
+		if (args.length > 0) {
+
+			// Add command-line interface here later.
+
+		} else {
+
+			// Put interactive interface here.
+
+		}
+
+	}
+
+}
